2f84fa83f9a18ccb409ef4be934dc08e3423f97d
Ganesh-14389 <gbhange18@gmail.com>
2025-06-03 15:56:01 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Ganesh/', // Add this line! Make sure it matches your repository name with leading and trailing slashes.
 })
